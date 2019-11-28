9e04c4295702b52bde71d9c9115bfe4b0a6700af
xp <xp@CETYSDAM00>
2019-11-28 18:17:49 +0100

    2o ejercicio

--- src/ejerciciosbasicos/java/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicos/java/EjerciciosBasicosJava.java
@@ -3,39 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejerciosbasicos.java;
-
-
+package ejerciciosbasicos.java;
 
 /**
-  @author Javier 
+ * @author Javier
  */
+public class EjerciciosBasicosJava {
 
-public class EjerciosBasicosJava {
-    
-    public boolean fiestaArdillas(int numBellotas, boolean finDeSemana){
-        if(finDeSemana){
-        return true;
+    public int multa(int velocidad, boolean birthday) {
+        if (birthday) {
+            velocidad -= 5;
         }
-        if (numBellotas >=40 && numBellotas <=60){
+        if (velocidad <= 60) {
+            return 0;
+        }
+        if (velocidad > 60 && velocidad <= 80) {
+            return 1;
+        }
+        return 2; //resto de casos
+    }
+        
+    public boolean fiestaArdillas(int numBellotas, boolean finDeSemana) {
+        if (finDeSemana) {
+            return true;
+        }
+        if (numBellotas >= 40 && numBellotas <= 60) {
             return true;
         }
         return false;
     }
-    public int multa (int velocidad, boolean birthday){
-        if(birthday){
-            velocidad -= 5;
-        }
-        if(velocidad <= 60){
-            return 0;
-        }
-        if(velocidad > 60 && velocidad <= 80){
-          return 1;  
-        }
-    return 2; //resto de casos
-    }
-        
-    
+
+
 
     /**
      * @param args the command line arguments
@@ -43,15 +41,16 @@ public class EjerciosBasicosJava {
     public static void main(String[] args) {
         // TODO code application logic here
         EjerciciosBasicosJava ejercicios = new EjerciciosBasicosJava();
+        
+        
+        
         System.out.println(ejercicios.fiestaArdillas(30, false));
         System.out.println(ejercicios.fiestaArdillas(50, true));
         System.out.println(ejercicios.fiestaArdillas(70, true));
-        //testeo ej 2
+
         System.out.println(ejercicios.multa(60, false));
         System.out.println(ejercicios.multa(65, false));
         System.out.println(ejercicios.multa(65, true));
-        
-        
     }
-    
+
 }
